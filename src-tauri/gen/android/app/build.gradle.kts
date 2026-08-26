ba690b65541506d0848684afac52e06253f7dead
tentaShiratori <tenta.shiratori@gmail.com>
2026-08-26 23:56:09 +0900

    chore: debugとreleaseでidentifierを変える

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
     }
     buildTypes {
         getByName("debug") {
+            applicationIdSuffix = ".debug"
             manifestPlaceholders["usesCleartextTraffic"] = "true"
             isDebuggable = true
             isJniDebuggable = true
